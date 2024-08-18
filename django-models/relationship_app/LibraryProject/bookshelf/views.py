9f7697bf1453fe52afded0e80f33e95ff3cc2745
selamawit Haile <selamhai18@gmail.com>
2024-08-18 22:08:40 +0300

    Implementing Advanced Model Relationships in Django

--- django-models/relationship_app/LibraryProject/bookshelf/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# bookshelf/views.py
-from django.http import HttpResponse
-
-def index(request):
-    #return HttpResponse("Welcome to the Bookshelf!")
-     return HttpResponse("Welcome to the Bookshelf!")
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library
 
-# Create your views here.
+
+
+#Implement Function-based View:
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Implementing the Class-Based View
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html' # Optional: Define a template for HTML rendering
+    context_object_name = 'library'
