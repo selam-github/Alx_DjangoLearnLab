b7443b7838f092d8f94e17ed2f73c25e072549ff
Selamawit Haile <selamhai18@gmail.com>
2024-08-23 02:46:39 -0700

    Implementing Custom Permissions in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,7 @@ from .views import list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views
 from . import views
 
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
@@ -12,4 +13,7 @@ urlpatterns = [
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),  #  for adding a book
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  #  for editing a book
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),  #  for deleting a book
 ]
