f48fedfda5ee279cf3caf3cbce5cbe27e47a1571
selamawit Haile <selamhai18@gmail.com>
2024-08-25 23:57:09 +0300

    Implementing Security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from .models import CustomUser
 from .models import Book
 from .forms import ExampleForm
+from .forms import BookSearchForm
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -34,5 +35,36 @@ def your_view(request):
     form = ExampleForm()
 
     return render(request, 'your_template.html', {'form': form})
+def search_books(request):
+    form = BookSearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data.get('query')
+        books = Book.objects.filter(title__icontains=query) if query else Book.objects.all()
+    else:
+        books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
+
+
+def your_view(request):
+    form = ExampleForm()
+    # Your view logic here
+    return render(request, 'your_template.html', {'form': form})
+
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data, e.g., create or update a model instance
+            title = form.cleaned_data['title']
+            author = form.cleaned_data['author']
+            publication_year = form.cleaned_data['publication_year']
+            # Example: Create a new Book instance (if applicable)
+            # Book.objects.create(title=title, author=author, publication_year=publication_year)
+            # Redirect or render a success page
+            return redirect('success_url')  # Update with your success URL
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 
