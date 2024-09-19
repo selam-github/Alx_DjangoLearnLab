e7fdc479bae590b97274854d35d7161f2508cba8
Selamawit Haile <selamhai18@gmail.com>
2024-09-19 04:21:14 -0700

    Blog's User Authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,16 +1,26 @@
-from django.urls import path ,include
-from blog import views as blog_views
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import RegisterView, profile_view
+from templates.blog import profile_edit 
+from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('login/', blog_views.user_login, name='login'),
-    path('logout/', blog_views.user_logout, name='logout'),
-    path('register/',blog_views.register, name='register'),
-    path('profile/', blog_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', profile_view, name='profile'),
+    path('profile/edit/', profile_edit, name='profile_edit'),
+
+    #List of posts (Read)
     path('', PostListView.as_view(), name='post_list'),
+     # View a specific post (Read)
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+     # Create a new post (Create)
     path('post/new/', PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
+     # Create a new post (Create)
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+    # Delete a specific post (Delete)
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
 
