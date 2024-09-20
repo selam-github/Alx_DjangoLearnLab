012d2f10dd594e5b6e3bb761a38bacedd5503d47
Selamawit Haile <selamhai18@gmail.com>
2024-09-20 05:02:06 -0700

    Setup and User Authentication for a Social Media API

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#Create a custom user model that extends Django’s AbstractUser, 
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import CustomUser
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate
 
-# Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class UserLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        return Response({'error': 'Invalid Credentials'}, status=400)
