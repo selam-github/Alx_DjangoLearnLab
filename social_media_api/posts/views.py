9a139904ca67b4bb2351acea654effe767d51239
Selamawit Haile <selamhai18@gmail.com>
2024-09-20 06:00:32 -0700

    Posts and Comments Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from django_filters.rest_framework import FilterSet, filters
+from django_filters import rest_framework as filters
 
-# Create your views here.
+# ViewSet for Post
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+# ViewSet for Comment
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filterset_class = PostFilter
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+ 
+  # # Define the PostFilter class for filtering posts     
+class PostFilter(FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+    content = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+
+
+    
