594a8cca0d1b139d607822ab71bb1725a14c4073
Selamawit Haile <selamhai18@gmail.com>
2024-09-20 02:30:15 -0700

    Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'password', 'bio', 'profile_picture', 'followers')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
