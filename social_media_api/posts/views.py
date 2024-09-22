d50a50a47594de778b71b436d6ae21c15525062b
selamawit Haile <selamhai18@gmail.com>
2024-09-22 22:02:08 +0300

    Notifications and Likes

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -26,7 +26,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filterset_class = PostFilter
+    
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
